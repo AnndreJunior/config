1ef2830ec8a563c62678a96de431b46dbfc4ebaf
AnndreJunior <anndre.junior15@gmail.com>
2025-03-03 20:52:02 -0300

    add neotest and debugger

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -27,6 +27,6 @@ keymap.set("n", "ss", ":split<Return>", opts)
 keymap.set("n", "sv", ":vsplit<Return>", opts)
 
 -- Diagnostics
-keymap.set("n", "<C-j>", function()
+keymap.set("n", "<C-k>", function()
   vim.diagnostic.goto_next()
 end, opts)
