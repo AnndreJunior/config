a215187e41c54730965c1d05dfd60636a2d7867e
André Júnior <138535113+AnndreJunior@users.noreply.github.com>
2025-03-03 22:31:03 +0000

    fix

--- README.md
+++
@@ -1,4 +0,0 @@
-# Setup
-```bash
-git clone https://github.com/AnndreJunior/config.git ~/.config
-```
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -131,7 +131,7 @@ return {
     },
   },
   {
-    "nvim-cmp",
+    "hrsh7th/nvim-cmp",
     dependencies = { "hrsh7th/cmp-emoji" },
     opts = function(_, opts)
       table.insert(opts.sources, { name = "emoji" })
